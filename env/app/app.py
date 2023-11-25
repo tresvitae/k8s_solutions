a2156e344153433eb6a324018f98f0e4ed21ba27
Patryk <patryk.futa@gmail.com>
2023-11-25 17:58:24 +0100

    Build api app using flask

---
+++ env/app/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import request
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/pods/env-var')
+def get_env_variable():
+    env_name = request.args.get('variable')
+    if env_name in os.environ:
+        return f"Hello {os.environ[env_name]}!"
+    else:
+        return "Hello anonymous!"
+    
+@app.route('/pods/env-vars')
+def get_env_variables():
+    return dict(os.environ)
+
+@app.route('/')
+def index():
+    return "Hello in Kubernetes course!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=3000)
